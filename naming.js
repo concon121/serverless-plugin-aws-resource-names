91289a687b9c18eac5519c6de087bac378d78cce
Connor Bray <connor.bray@andoverlab.co.uk>
2017-11-30 09:48:13 +0000

    Updated naming for lambda log groups

--- naming.js
+++ naming.js
@@ -5,11 +5,17 @@ const _ = require('lodash')
 
 module.exports = {
 
-    _getMappings() {
+    _getMappings(lambdaName) {
         var data = fs.readFileSync(this.provider.serverless.service.custom['serverless-aws-resource-names'].source, 'utf8')
         data = data.replace(new RegExp('\\$stage', 'g'), this.provider.getStage() || 'dev')
         data = data.replace(new RegExp('\\$region', 'g'), this.provider.getRegion())
         data = data.replace(new RegExp('\\$service', 'g'), this.provider.serverless.service.service)
+        if (lambdaName) {
+            data = data.replace(new RegExp('\\$lambda', 'g'), lambdaName)
+        } else {
+            data = data.replace(new RegExp('\\$lambda', 'g'), 'lambdaName')
+        }
+        console.log(data)
         return JSON.parse(data)
     },
 
@@ -32,30 +38,30 @@ module.exports = {
     getApiGatewayName() {
         return this._getMappings().apiGateway
     },
-    getLogGroupName() {
-        return this._getMappings().logGroup
+    getLogGroupName(functionName) {
+        const self = this
+        var logGroup
+        _.forEach(self.provider.serverless.service.functions, (functionObj, name) => {
+            if (JSON.stringify(functionName).includes(name)) {
+                logGroup = this._getMappings(name).logGroup
+                return false
+            }
+        })
+        return logGroup
     },
     setFunctionNames(provider) {
         const self = this
+        var mappings
         if (!self.provider) {
             self.provider = provider
         }
         if (self.provider) {
-            const mappings = self._getMappings()
             _.forEach(self.provider.serverless.service.functions, (functionObj, functionName) => {
                 if (!functionObj.events) {
                     self.provider.serverless.service.functions[functionName].events = []
                 }
-                if (typeof mappings.lambda === 'object') {
-                    for (var property in mappings.lambda) {
-                        if (mappings.lambda.hasOwnProperty(property)) {
-                            mappings.lambda[property] = mappings.lambda[property].replace('$lambda', functionName)
-                        }
-                    }
-                    self.provider.serverless.service.functions[functionName].name = mappings.lambda
-                } else {
-                    self.provider.serverless.service.functions[functionName].name = mappings.lambda.replace('$lambda', functionName)
-                }
+                mappings = self._getMappings(functionName)
+                self.provider.serverless.service.functions[functionName].name = mappings.lambda
             })
         }
     }
